1c12ac6462a727d52cf272dfd851d6f86e1205aa
Asia-parveen <beautyhealthytips@gmail.com>
2024-04-21 04:11:38 -0700

    added files

---
+++ app.js
@@ -0,0 +1,33 @@
+// 1. Write a script to greet your website visitor using JS alert
+// box?
+
+alert("Thanx for visiting my website");
+
+// 2. Write a script to display following message on your web
+// page:?
+
+alert("JavaScript Alert \n \n Error! Please enter a valid password. ");
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)?
+
+alert("JavaScript Alert! \n \n Welcome to JS Land...\n Happy Coading");
+
+// 4. Write a script to display following messages in sequence:?
+
+alert("JavaScript Alert \n \n Welcome to JS Land... ");
+
+alert("Happy Coding! \n \nPrevent this page from creating additional dialogs.");
+
+// 5. Generate the following message through browser’s
+// developer console:?
+
+alert(" JavaScript Alert \n \n Hello... I can run JS  through my web browser's console");
+
+// 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)?
+
+done
+
