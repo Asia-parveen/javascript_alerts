031a04ed68a52e1986453fc9f5c22da3193119f9
Asia-parveen <beautyhealthytips@gmail.com>
2024-05-04 09:51:24 -0700

    modification

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 // 1. Write a script to greet your website visitor using JS alert
 // box?
 
-alert("Thanx for visiting my website");
+// alert("Thanx for visiting my website");
 
 // 2. Write a script to display following message on your web
 // page:?
 
-alert("JavaScript Alert \n \n Error! Please enter a valid password. ");
+//alert("JavaScript Alert \n \n Error! Please enter a valid password. ");
 
 // 3. Write a script to display following message on your web
 // page: (Hint : Use line break)?
 
-alert("JavaScript Alert! \n \n Welcome to JS Land...\n Happy Coading");
+//alert("JavaScript Alert! \n \n Welcome to JS Land...\n Happy Coading");
 
 // 4. Write a script to display following messages in sequence:?
 
-alert("JavaScript Alert \n \n Welcome to JS Land... ");
+//alert("JavaScript Alert \n \n Welcome to JS Land... ");
 
-alert("Happy Coding! \n \nPrevent this page from creating additional dialogs.");
+//alert("Happy Coding! \n \nPrevent this page from creating additional dialogs.");
 
 // 5. Generate the following message through browser’s
 // developer console:?
 
-alert(" JavaScript Alert \n \n Hello... I can run JS  through my web browser's console");
+//alert(" JavaScript Alert \n \n Hello... I can run JS  through my web browser's console");
 
 // 7. Practice placement of <script></script> element in
 // following sections of your project in exercise 6:
